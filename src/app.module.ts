26092fec06618302643570aa48f0468575c2379d
Olisa Agbafor <olisaagbafor@gmail.com>
2025-03-22 14:42:16 +0100

    fix: register all related entities in CoursesModule - Remove duplicate module imports, add missing entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { CoursesModule } from './courses/courses.module';
 import { AuthModule } from './auth/auth.module';
-import { CourseModule } from './course/course.module';
-import { ModuleModule } from './module/module.module';
-import { LessonModule } from './lesson/lesson.module';
 import { CertificateModule } from './certificate/certificate.module';
 import { ForumModule } from './forum/forum.module';
 import { PaymentModule } from './payment/payment.module';
@@ -38,30 +35,15 @@ import { EmailModule } from './email/email.module';
     }),
 
     UsersModule,
-
     CoursesModule,
-
     AuthModule,
-
-    CourseModule,
-
-    ModuleModule,
-
-    LessonModule,
-
     CertificateModule,
-
     ForumModule,
-
     PaymentModule,
-
     NotificationModule,
-
     BlockchainModule,
-
     FilesModule,
-
     EmailModule,
   ],
 })
-export class AppModule {}
+export class AppModule { }
--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -2,17 +2,28 @@ import { Module } from '@nestjs/common';
 
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Course } from './entities/course.entity';
+import { CourseModule } from './entities/course-module.entity';
+import { Category } from './entities/category.entity';
+import { Tag } from './entities/tag.entity';
+import { CourseReview } from './entities/course-review.entity';
 import { CourseService } from './courses.service';
 import { CourseController } from './courses.controller';
 import { User } from 'src/users/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Course,User])],
+  imports: [TypeOrmModule.forFeature([
+    Course,
+    CourseModule,
+    Category,
+    Tag,
+    CourseReview,
+    User
+  ])],
   providers: [CourseService],
   controllers: [CourseController],
   exports: [CourseService],
 })
-export class CoursesModule {}
+export class CoursesModule { }
 
 
 
