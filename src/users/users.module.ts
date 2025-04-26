d3dd2254a17b18f5c89604eb251c1d817a44ff75
kamaldeen <kamalaliyu503@gmail.com>
2025-04-26 11:05:28 +0100

    Implement Course Progress Tracking Feature #74

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,21 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
+import { UserProgress } from './entities/user-progress.entity';
+import { ProgressService } from './services/progress.service';
+import { ProgressController } from './controllers/progress.controller';
+import { Course } from '../courses/entities/course.entity';
+import { Lesson } from '../lesson/entity/lesson.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  controllers: [UsersController],
-  providers: [UsersService],
-  exports: [UsersService],
+  imports: [
+    TypeOrmModule.forFeature([User, UserProgress, Course, Lesson]),
+  ],
+  controllers: [UsersController, ProgressController],
+  providers: [UsersService, ProgressService],
+  exports: [UsersService, ProgressService],
 })
 export class UsersModule {}
 
