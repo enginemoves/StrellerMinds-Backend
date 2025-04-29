5a67f95a8d6226f06719ba4e72e2e5bc51830515
DIVINE IFEDIORAH <ifediorahchinonso5@gmail.com>
2025-04-29 15:10:01 +0100

    Admin API course CRUD

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from "@nestjs/common";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
+import { Roles } from "src/auth/decorators/roles.decorator";
+import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
+import { CreateCourseDto } from "src/courses/dtos/create.course.dto";
+import { RolesGuard } from "src/auth/guards/roles.guard"; 
+import { CourseService } from "./course.service";
+import { UpdateCourseDto } from "src/courses/dtos/update.course.dto";
 
-@Controller('course')
-export class CourseController {}
+@ApiTags('Courses')
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles('admin')
+@Controller('courses')
+
+export class CoursesController {
+     constructor(private readonly service: CourseService) {}
+
+     @Post()
+     @ApiOperation({ summary: 'Create a course' })
+     create(@Body() dto: CreateCourseDto) {
+          return this.service.create(dto);
+     }
+
+     @Get()
+     @ApiOperation({ summary: 'Get all courses' })
+     findAll() {
+          return this.service.findAll();
+     }
+
+     @Get(':id')
+     @ApiOperation({ summary: 'Get course by ID' })
+     findOne(@Param('id') id: string) {
+          return this.service.findOne(id);
+     }
+
+     @Patch(':id')
+     @ApiOperation({ summary: 'Update course' })
+     update(@Param('id') id: string, @Body() dto: UpdateCourseDto) {
+          return this.service.update(id, dto);
+     }
+
+     @Delete(':id')
+     @ApiOperation({ summary: 'Delete course' })
+     remove(@Param('id') id: string) {
+          return this.service.remove(id);
+     }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { RolesGuard } from './role/roles.guard';
 import { GlobalExceptionsFilter } from './common/filters/global-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,17 @@ async function bootstrap() {
   // Use the exception filter globally
   app.useGlobalFilters(new GlobalExceptionsFilter());
 
+  const config = new DocumentBuilder()
+    .setTitle('StrellerMinds API')
+    .setDescription('Admin API for course management')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
