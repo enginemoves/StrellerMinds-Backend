0e49bc14fcbda7441afeed1af74b8c80ca803ea8
Daniel Job Gonsum <danieljob003@gmail.com>
2025-05-31 10:43:26 +0100

    feat(course): implement basic CRUD methods in CourseService

--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Course } from './course.entity';
+import { CreateCourseDto, UpdateCourseDto } from './course.dto';
 
 @Injectable()
-export class CourseService {}
+export class CourseService {
+  constructor(
+    @InjectRepository(Course)
+    private readonly courseRepository: Repository<Course>,
+  ) {}
+
+  async create(createCourseDto: CreateCourseDto): Promise<Course> {
+    const course = this.courseRepository.create(createCourseDto);
+    return this.courseRepository.save(course);
+  }
+
+  async findAll(): Promise<Course[]> {
+    return this.courseRepository.find();
+  }
+
+  async findOne(id: number): Promise<Course> {
+    const course = await this.courseRepository.findOneBy({ id });
+    if (!course) {
+      throw new NotFoundException(`Course with ID ${id} not found`);
+    }
+    return course;
+  }
+
+  async update(id: number, updateCourseDto: UpdateCourseDto): Promise<Course> {
+    const course = await this.findOne(id);
+    Object.assign(course, updateCourseDto);
+    return this.courseRepository.save(course);
+  }
+
+  async remove(id: number): Promise<void> {
+    const course = await this.findOne(id);
+    await this.courseRepository.remove(course);
+  }
+
+  // Bulk operations can be added later when ready
+}
