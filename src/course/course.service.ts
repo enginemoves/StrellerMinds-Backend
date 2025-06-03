6653b08887297c363ab0c160877cc58c8a4de26a
Daniel Job Gonsum <danieljob003@gmail.com>
2025-06-03 11:01:46 +0100

    Update course.service.ts

--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Course } from './course.entity';
 
 @Injectable()
 export class CourseService {
-  getAllCourses() {
-    return 'List of all courses';
+  constructor(
+    @InjectRepository(Course)
+    private readonly courseRepository: Repository<Course>,
+  ) {}
+
+  async create(dto: Partial<Course>): Promise<Course> {
+    const course = this.courseRepository.create(dto);
+    return this.courseRepository.save(course);
+  }
+
+  async findAll(): Promise<Course[]> {
+    return this.courseRepository.find();
   }
 }
