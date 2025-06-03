e133bb9994c434ed6c7380bfb2d48c5a5ca25115
Daniel Job Gonsum <danieljob003@gmail.com>
2025-06-03 10:45:29 +0100

    Update course.controller.ts

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -1,3 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+import { CourseService } from './course.service';
+
+@Controller('courses')
+export class CourseController {
+  constructor(private readonly courseService: CourseService) {}
+
+  @Get()
+  getAllCourses() {
+    return this.courseService.getAllCourses();
+  }
+}
 
 
 
