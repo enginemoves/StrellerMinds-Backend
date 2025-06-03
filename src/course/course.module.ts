a17b51072730b2fe495b28b7ed5ed56cbc83e2db
Daniel Job Gonsum <danieljob003@gmail.com>
2025-06-03 10:47:30 +0100

    Update course.module.ts

--- src/course/course.module.ts
+++ src/course/course.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CourseService } from './course.service';
+import { CourseController } from './course.controller';
 
 @Module({
+  controllers: [CourseController],
   providers: [CourseService],
   exports: [CourseService],
 })
